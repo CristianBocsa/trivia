f7646dd7cbc53ffcf924e3c25e80e81a8f707240
CristianBocsa <84340298+CristianBocsa@users.noreply.github.com>
2021-05-18 22:27:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,79 +4,79 @@ import json
 
 root = Tk()
 root.geometry("800x500")
-root.title("Quiz")
-with open('quiz.json') as f:
-    obj = json.load(f)
-q = (obj['ques'])
-options = (obj['options'])
-a = (obj['ans'])
+root.title("Bocsa Cristian, Magda Alexandru")
+with open('intrebari.json') as f:
+    x = json.load(f)
+intrebari = (x['intr'])
+variante = (x['var'])
+raspunsuri = (x['rasp'])
 
 
 class Quiz:
     def __init__(self):
-        self.qn = 0
-        self.ques = self.question(self.qn)
-        self.opt_selected = IntVar()
-        self.opts = self.radiobtns()
-        self.display_options(self.qn)
-        self.buttons()
-        self.correct = 0
+        self.intreb = 0
+        self.ques = self.intrebari(self.intreb)
+        self.opt_selectata = IntVar()
+        self.opts = self.cerculete()
+        self.display(self.intreb)
+        self.butoane()
+        self.corect = 0
 
-    def question(self, qn):
-        t = Label(root, text="Quiz in Python Programming", width=50, bg="blue", fg="white", font=("times", 20, "bold"))
-        t.place(x=0, y=2)
-        qn = Label(root, text=q[qn], width=60, font=("times", 16, "bold"), anchor="w")
+    def intrebari(self, qn):
+        t = Label(root, text="Triviador", width=48, bg="black", fg="white", font=("arial", 20, "bold", "italic"))
+        t.place(x=0, y=5)
+        qn = Label(root, text=intrebari[qn], width=60, fg="black", font=("times", 16, "bold"), anchor="center")
         qn.place(x=70, y=100)
         return qn
 
-    def radiobtns(self):
+    def cerculete(self):
         val = 0
         b = []
         yp = 150
         while val < 4:
-            btn = Radiobutton(root, text=" ", variable=self.opt_selected, value=val + 1, font=("times", 14))
+            btn = Radiobutton(root, text="", variable=self.opt_selectata, value=val + 1, font=("arial", 15, "bold"))
             b.append(btn)
-            btn.place(x=100, y=yp)
-            val += 1
-            yp += 40
+            btn.place(x=50, y=yp)
+            val = val + 1
+            yp = yp + 40
         return b
 
-    def display_options(self, qn):
+    def display(self, qn):
         val = 0
-        self.opt_selected.set(0)
-        self.ques['text'] = q[qn]
-        for op in options[qn]:
+        self.opt_selectata.set(0)
+        self.ques['text'] = intrebari[qn]
+        for op in variante[qn]:
             self.opts[val]['text'] = op
             val += 1
 
-    def buttons(self):
-        nbutton = Button(root, text="Next", command=self.nextbtn, width=10, bg="green", fg="white",
-                         font=("times", 16, "bold"))
-        nbutton.place(x=200, y=380)
-        quitbutton = Button(root, text="Quit", command=root.destroy, width=10, bg="red", fg="white",
-                            font=("times", 16, "bold"))
-        quitbutton.place(x=380, y=380)
+    def butoane(self):
+        urmator = Button(root, text="Urmatoarea intrebare", command=self.but_urm, width=20, bg="green", fg="black",
+                         font=("times", 16, "bold"), anchor="center")
+        urmator.place(x=275, y=380)
+        exit = Button(root, text="Quit", command=root.destroy, width=10, bg="red", fg="black",
+                            font=("times", 16, "bold"), anchor="center")
+        exit.place(x=335, y=430)
 
-    def checkans(self, qn):
-        if self.opt_selected.get() == a[qn]:
+    def validare(self, qn):
+        if self.opt_selectata.get() == raspunsuri[qn]:
             return True
 
-    def nextbtn(self):
-        if self.checkans(self.qn):
-            self.correct += 1
-        self.qn += 1
-        if self.qn == len(q):
-            self.display_result()
+    def but_urm(self):
+        if self.validare(self.intreb):
+            self.corect =self.corect + 1
+        self.intreb = self.intreb + 1
+        if self.intreb == len(intrebari):
+            self.display_rezultate()
         else:
-            self.display_options(self.qn)
+            self.display(self.intreb)
 
-    def display_result(self):
-        score = int(self.correct / len(q) * 100)
-        result = "Score: " + str(score) + "%"
-        wc = len(q) - self.correct
-        correct = "No. of correct answers: " + str(self.correct)
-        wrong = "No. of wrong answers: " + str(wc)
-        mb.showinfo("Result", "\n".join([result, correct, wrong]))
+    def display_rezultate(self):
+        scor = int(self.corect / len(intrebari) * 100)
+        rezultat = "Procentaj raspunsuri : " + str(scor) + "%"
+        ramase = len(intrebari) - self.corect
+        correct = "Numarul raspunsurilor corecte: " + str(self.corect)
+        gresit = "Numarul raspunsurilor gresite: " + str(ramase)
+        mb.showinfo("Rezultat", "\n".join([rezultat, correct, gresit]))
 
 
 quiz = Quiz()
